440f10d9011b71dc7c65a45ab222e8b9245ffb72
eric-rutgers <149545453+eric-rutgers@users.noreply.github.com>
2023-11-01 12:26:23 -0400

    Add files via upload

---
+++ completion.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE completionset
+    DEFAULT CHARACTER SET = 'utf8mb4';
+
+
+CREATE table Completion(
+  Completion_id INT PRIMARY KEY,
+  Completion_Confirmation BOOLEAN,
+  Opinion_Query VARCHAR(255) NOT NULL,
+  FOREIGN KEY (User_id) REFERENCES Users(User_id),
+  FOREIGN KEY (Tracker_id) REFERENCES tracker(Tracker_id),
+  FOREIGN KEY (Product_id) REFERENCES Products(Product_id)   
+) COMMENT '';
