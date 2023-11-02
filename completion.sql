45cce3d15eb258e5a1ecf341e83dd41c76a9a7ec
eric-rutgers <149545453+eric-rutgers@users.noreply.github.com>
2023-11-01 23:32:59 -0400

    Updated completion.sql

--- completion.sql
+++ completion.sql
@@ -7,6 +7,6 @@ CREATE table Completion(
   Completion_Confirmation BOOLEAN,
   Opinion_Query VARCHAR(255) NOT NULL,
   FOREIGN KEY (User_id) REFERENCES Users(User_id),
-  FOREIGN KEY (Tracker_id) REFERENCES tracker(Tracker_id),
+  FOREIGN KEY (Tracker_id) REFERENCES Tracker(Tracker_id),
   FOREIGN KEY (Product_id) REFERENCES Products(Product_id)   
 ) COMMENT '';
