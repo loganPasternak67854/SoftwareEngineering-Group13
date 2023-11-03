8943356eb0a8a1105653496b703447bb224eac7e
santosh <santosh.pandey2222@gmail.com>
2023-11-03 11:51:26 -0400

    fixes error from login and signup function missing (

--- login-handle.js
+++ login-handle.js
@@ -7,42 +7,43 @@ const loginSuccessMsg = document.getElementById("login-success-msg");
 const signUpButton = document.getElementById("signup-button");
 
 loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    const username = loginForm.username.value;
-    const password = loginForm.password.value;
-    loginErrorMsg.style.opacity = 0;
-    if (username === "user" && password === "pass") {
-        // alert("You have successfully logged in.");
-        loginSuccessMsg.style.opacity = 1;
-        setTimeout(3000);
-        location.replace('/monitor.html');
-    } else {
-        loginErrorMsg.style.opacity = 1;
-    }
-})
+  e.preventDefault();
+  console.log("login button clicked");
+  const username = loginForm.username.value;
+  const password = loginForm.password.value;
+  loginErrorMsg.style.opacity = 0;
+  if (username === "user" && password === "pass") {
+      // alert("You have successfully logged in.");
+      loginSuccessMsg.style.opacity = 1;
+      setTimeout(3000);
+      location.replace('/monitor.html');
+  } else {
+      loginErrorMsg.style.opacity = 1;
+  }
+});
 
 loginButton.addEventListener("click", (e) => {
-    e.preventDefault();
-    if (checkExistingUser(username, email)) {
-        return "User already exists. Please choose a different username or email.";
-    }
-    const newUser = {
-        username: username,
-        email: email,
-        password: password,
-    };
-    userAccounts.push(newUser);
-    // alert("Account successfully created.");
-}
+  e.preventDefault();
+  if (checkExistingUser(username, email)) {
+      return "User already exists. Please choose a different username or email.";
+  }
+  const newUser = {
+      username: username,
+      email: email,
+      password: password,
+  };
+  userAccounts.push(newUser);
+  // alert("Account successfully created.");
+});
 
 function checkExistingUser(username, email) {
-  for (let user of userAccounts) {
-    if (user.username === username || user.email === email) {
-      return true; 
-    }
+for (let user of userAccounts) {
+  if (user.username === username || user.email === email) {
+    return true; 
   }
+}
 
-  return false; 
+return false; 
 }
 
 const userAccounts = []; // Array to hold user accounts (temporary)
