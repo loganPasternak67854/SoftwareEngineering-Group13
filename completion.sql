3f24303324cc5ddd1d8a5eae32f73289ed01d06d
eric-rutgers <149545453+eric-rutgers@users.noreply.github.com>
2023-11-02 22:48:11 -0400

    Update completion.sql

--- completion.sql
+++ completion.sql
@@ -1,6 +1,30 @@
 CREATE DATABASE completionset
     DEFAULT CHARACTER SET = 'utf8mb4';
+CREATE TABLE Users (
+    User_id INT PRIMARY KEY,
+    Name VARCHAR(255) NOT NULL,
+    Address VARCHAR(255),
+    Email_Address VARCHAR(255),
+    Phone_Number VARCHAR(20)
+);
 
+CREATE TABLE Products (
+ Product_id INT PRIMARY KEY,
+    Product_name VARCHAR(255) NOT NULL,
+    Product_description TEXT,
+    Discount_category VARCHAR(50)
+);
+
+CREATE TABLE Tracking (
+    Tracker_id INT PRIMARY KEY,
+    User_id INT,
+    Product_id INT,
+    Order_status BOOLEAN,
+    Current_shipping_status VARCHAR(50),
+    Shipping_provider VARCHAR(100),
+    FOREIGN KEY (User_id) REFERENCES Users(User_id),
+    FOREIGN KEY (Product_id) REFERENCES Products(Product_id) 
+);
 
 CREATE table Completion(
   Completion_id INT PRIMARY KEY,
