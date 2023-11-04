2361a113a3275bc75d8d664cf28be595207a78d2
aniketAnvekar <aniketanvekar25@gmail.com>
2023-11-03 20:30:14 -0400

    Integrated Add to Cart functionality

--- GoodsWebsiteDatabase.sql
+++ GoodsWebsiteDatabase.sql
@@ -1,6 +1,7 @@
-USE GoodsWebsiteDatabase;--Contributed by Logan Pasternak
+CREATE DATABASE GoodsWebsiteDatabase;
+USE GoodsWebsiteDatabase;
 DEFAULT CHARACTER SET = 'utf8mb4';
---This is the user entity 
+
 CREATE TABLE USERS(
     userID INT AUTO_INCREMENT PRIMARY KEY, 
     userName VARCHAR(255) NOT NULL,
@@ -11,26 +12,32 @@ CREATE TABLE USERS(
     userPhone VARCHAR(255) UNIQUE NOT NULL, 
     trackingCODE VARCHAR(6) UNIQUE NOT NULL 
 );
---The information regarding a single product is tracked here
+
 CREATE TABLE PRODUCTS(
     productID INT AUTO_INCREMENT PRIMARY KEY,
     productName VARCHAR(255) NOT NULL, 
     productDescription TEXT, 
     discountCategory ENUM('ENABLED','DISABLED') NOT NULL,
     discountAmount INT, 
-    productPrice DECIMAL(10,2) NOT NULL
+    productPrice DECIMAL(10,2) NOT NULL,
+    imageURL VARCHAR(255) NOT NULL
 );
 
---The collection of products in the cart
+CREATE INDEX productName_index ON PRODUCTS (productName);
+CREATE INDEX imageURL_index on PRODUCTS (imageURL);
+
 CREATE TABLE CART(
     cartID INT AUTO_INCREMENT PRIMARY KEY, 
     userID INT, 
     productID INT, 
+    productName VARCHAR(255) NOT NULL, 
+    imageURL VARCHAR(255) NOT NULL,
     numProducts INT NOT NULL,
     totalPrice DECIMAL(10,2) NOT NULL,
     FOREIGN KEY (userID) REFERENCES USERS(userID), 
-    FOREIGN KEY (productID) REFERENCES PRODUCTS(productID)
-     
+    FOREIGN KEY (productID) REFERENCES PRODUCTS(productID),
+	FOREIGN KEY (productName) REFERENCES PRODUCTS(productName),
+    FOREIGN KEY (imageURL) REFERENCES PRODUCTS(imageURL)
 );
 
 CREATE TABLE CreditCard (
@@ -42,7 +49,7 @@ CREATE TABLE CreditCard (
     FOREIGN KEY (userID) REFERENCES USERS(userID)
 );
 
---Package tracking database
+
 CREATE TABLE TRACKING(
     trackerID INT AUTO_INCREMENT PRIMARY KEY, 
     userID INT, 
@@ -52,7 +59,7 @@ CREATE TABLE TRACKING(
     shippingProvider VARCHAR(255) NOT NULL 
 );
 
---Order completion database
+
 CREATE TABLE COMPLETE(
     completionID INT AUTO_INCREMENT PRIMARY KEY,
     trackerID INT, 
@@ -89,7 +96,6 @@ DELETE FROM TRACKING;
 
 DELETE FROM COMPLETE;
 
---Puts data into Users table
 INSERT INTO USERS (userName,loginUsername,loginPassword,userAddress,userEmail,userPhone,trackingCode)
 VALUES
     ('Alice','temp','temp', '123 Elm St', 'alice@example.com', '555-123-4567', 'A1B2C3'),
@@ -97,15 +103,13 @@ VALUES
     ('Michael','tubularBells','password', '789 Pine Rd', 'michael@example.com', '555-555-5555', 'M9P8QZ'),
     ('Emily','CocoPuffs','josh918', '101 Redwood Ave', 'emily@gmail.com', '111-222-3333', 'E7R2W1');
 
---Puts data into Products table
-INSERT INTO PRODUCTS(productName,productDescription,discountCategory, discountAmount, productPrice)
+INSERT INTO PRODUCTS(productName,productDescription,discountCategory, discountAmount, productPrice,imageURL)
 VALUES
-    ('Laptop', 'High-performance laptop', 'DISABLED', 20, 799.99),
-    ('Coffee Maker', 'Automatic coffee machine', 'ENABLED', 0, 49.99),
-    ('Smartphone', 'High-end mobile device', 'DISABLED', 60, 499.99),
-    ('Headphones', 'Wireless noise-canceling headphones', 'DISABLED', 30, 149.99);
+    ('Laptop', 'High-performance laptop', 'DISABLED', 20, 799.99,'assets/laptop.jpg'),
+    ('Coffee Maker', 'Automatic coffee machine', 'ENABLED', 0, 49.99,'assets/coffeemaker.jpg'),
+    ('Smartphone', 'High-end mobile device', 'DISABLED', 60, 499.99,'assets/smartphone.jpg'),
+    ('Headphones', 'Wireless noise-canceling headphones', 'DISABLED', 30, 149.99,'assets/headphones.jpg');
 
---Puts data into Cart table
 INSERT INTO CART(userID,productID,numProducts, totalPrice)
 VALUES
     (17,7,2,10.00),
@@ -113,9 +117,6 @@ VALUES
     (18,8,1,499.99),
     (19,9,1,149.99);
 
-
-
---Puts data into Tracking table
 INSERT INTO TRACKING(userID,orderStatus,shippingStatus,shippingProvider)
 VALUES
     (17,'IN PROGRESS','ASSEMBLY','HoalinOats'),
@@ -123,7 +124,6 @@ VALUES
     (19,'CANCELED','COMPLETE','temp'),
     (20,'NO ORDER', 'COMPLETE', 'temp');
 
---Puts data into Complete table
 INSERT INTO COMPLETE(trackerID, completionMessage, completionConfirmation, opinionQuery)
 VALUES
     (14,'PACKAGE ON ITS WAY','INCOMPLETE','6'),
@@ -131,19 +131,12 @@ VALUES
     (16,'NaN','INCOMPLETE','1'),
     (17,'NaN','INCOMPLETE','4');
 
---Show User data in a table format
 SELECT * FROM USERS
 
---Show Product data in a table format
 SELECT * FROM PRODUCTS
 
---Show Cart data associated with a specific user
 SELECT * FROM CART
 
---Show Tracking data associated with a specific user
 SELECT * FROM TRACKING
 
---Show completion data associated with a specific user
 SELECT * FROM COMPLETION
-
-//CONTRIBUTION OF LOGAN PASTERNAK END
--- basicServer.js
+++ basicServer.js
@@ -1,111 +1,211 @@
-// const express = require('express');
+import {
+	createUSER,
+	createPRODUCT,
+	createCART,
+	createTRACKING,
+	createCOMPLETION,
+} from './database.js';
+import {
+	getUSERS,
+	getUSER,
+	getUSERNAME,
+	getEMAIL,
+	getPHONE,
+	getADDRESS,
+	getCODE,
+} from './database.js';
+import {
+	getPRODUCTS,
+	getPRODUCT,
+	getPRODUCT_NAME,
+	getPRODUCT_DESCRIPTION,
+	getPRODUCT_DISCOUNT,
+	getPRODUCT_DISCOUNT_AMOUNT,
+	getPRODUCT_PRICE,
+} from './database.js';
+import {
+	getCARTS,
+	getCART,
+	getCART_NUMPRODUCTS,
+	getCART_TOTAL_PRICE,
+} from './database.js';
+import {
+	getTRACKINGS,
+	getTRACKING,
+	getTRACKING_ORDERSTATUS,
+	getTRACKING_SHIPPINGSTATUS,
+	getTRACKING_SHIPPINGPROVIDER,
+} from './database.js';
+import {
+	getCOMPLETES,
+	getCOMPLETE,
+	getCOMPLETE_COMPLETIONMESSAGE,
+	getCOMPLETE_COMPLETIONCONFIRMATION,
+	getCOMPLETE_OPINIONQUERY,
+} from './database.js';
+import {
+	deleteUSER,
+	deletePRODUCT,
+	deleteCART,
+	deleteTRACKING,
+	deleteCOMPLETION,
+} from './database.js';
+import {
+	updateUSER,
+	updateUSERNAME,
+	updateADDRESS,
+	updateEMAIL,
+	updatePHONE,
+	updateCODE,
+} from './database.js';
+import {
+	updatePRODUCT,
+	updatePRODUCT_DESCRIPTION,
+	updatePRODUCT_NAME,
+	updatePRODUCT_DISCOUNT,
+	updatePRODUCT_DISCOUNT_AMOUNT,
+	updatePRODUCT_PRICE,
+} from './database.js';
+import {
+	updateCART,
+	updateCART_NUMPRODUCTS,
+	updateCART_TOTALPRICE,
+} from './database.js';
+import {
+	updateTRACKING_ORDERSTATUS,
+	updateTRACKING_SHIPPINGSTATUS,
+	updateTRACKING_SHIPPINGPROVIDER,
+} from './database.js';
+import {
+	updateCOMPLETE,
+	updateCOMPLETE_MESSAGE,
+	updateCOMPLETE_CONFIRMATION,
+	updateCOMPLETE_QUERY,
+} from './database.js';
 
-//CONTRIBUTION OF LOGAN PASTERNAK START
-
-import { createUSER, createPRODUCT, createCART, createTRACKING, createCOMPLETION } from "./database.js";
-import { getUSERS, getUSER, getUSERNAME, getUSER_LOGIN_USERNAME,getUSER_LOGIN_PASSWORD, getEMAIL, getPHONE, getADDRESS, getCODE } from "./database.js";
-import { getPRODUCTS, getPRODUCT, getPRODUCT_NAME, getPRODUCT_DESCRIPTION, getPRODUCT_DISCOUNT, getPRODUCT_DISCOUNT_AMOUNT, getPRODUCT_PRICE } from "./database.js";
-import { getCARTS, getCART, getCART_NUMPRODUCTS, getCART_TOTAL_PRICE } from "./database.js";
-import { getTRACKINGS, getTRACKING, getTRACKING_ORDERSTATUS, getTRACKING_SHIPPINGSTATUS, getTRACKING_SHIPPINGPROVIDER } from "./database.js";
-import { getCOMPLETES, getCOMPLETE, getCOMPLETE_COMPLETIONMESSAGE, getCOMPLETE_COMPLETIONCONFIRMATION, getCOMPLETE_OPINIONQUERY } from "./database.js";
-import { deleteUSER, deletePRODUCT, deleteCART, deleteTRACKING, deleteCOMPLETION } from "./database.js";
-import { updateUSER, updateUSERNAME, updateUSER_LOGIN_USERNAME, updateUSER_LOGIN_PASSWORD, updateADDRESS, updateEMAIL, updatePHONE, updateCODE } from "./database.js";
-import { updatePRODUCT, updatePRODUCT_DESCRIPTION, updatePRODUCT_NAME, updatePRODUCT_DISCOUNT, updatePRODUCT_DISCOUNT_AMOUNT, updatePRODUCT_PRICE } from "./database.js";
-import { updateCART, updateCART_NUMPRODUCTS, updateCART_TOTALPRICE } from "./database.js";
-import { updateTRACKING_ORDERSTATUS, updateTRACKING_SHIPPINGSTATUS, updateTRACKING_SHIPPINGPROVIDER } from "./database.js";
-import { updateCOMPLETE, updateCOMPLETE_MESSAGE, updateCOMPLETE_CONFIRMATION, updateCOMPLETE_QUERY } from "./database.js";
-import { getCREDITCARD, getCREDITCARD_SINGLE, getCREDITCARD_NUMBER, getCREDITCARD_EXPIRATION, getCREDITCARD_CVV } from "./database.js";
-import { createCREDITCARD } from "./database.js";
-import { deleteCREDITCARD } from "./database.js";
-import { updateCREDITCARD, updateCREDITCARD_CARDNUMBER, updateCREDITCARD_EXPIRATION, updateCREDITCARD_CVV } from "./database.js";
-
-//CONTRIBUTION OF LOGAN PASTERNAK ENDS
-
-//CONTRIBUTION OF Rumaysa Adnan
-
-// const express = require('express');
-
-import express from 'express'
+import express from 'express';
 import fs from 'fs';
-
 
 const app = express();
 const port = 3002;
-app.use(express.static("."));
-// Start the server
+app.use(express.json());
+app.use(express.static('.'));
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+	console.log(`Server is running on http://localhost:${port}`);
 });
-//CONTRIBUTION OF LOGAN PASTERNAK END
 
-
-//CONTRIBUTION OF Santosh Pandey START
 app.get('/', (req, res) => {
-  res.sendFile('index.html', { root: '.' });
+	res.sendFile('index.html', { root: '.' });
+});
+
+app.get('/products', async (req, res) => {
+	try {
+		const products = await getPRODUCTS();
+		res.json(products);
+	} catch (error) {
+		res.status(500).json({ error: 'Internal Server Error' });
+	}
+});
+
+app.get('/getcart', async (req, res) => {
+	try {
+		const carts = await getCARTS();
+		res.json(carts);
+	} catch (error) {
+		res.status(500).json({ error: 'Internal Server Error' });
+	}
+});
+
+app.post('/addtocart', async (req, res) => {
+	try {
+		console.log(req.body);
+		const {
+			userID,
+			productID,
+			productName,
+			numProducts,
+			totalPrice,
+			imageURL,
+		} = req.body;
+		await createCART(
+			userID,
+			productID,
+			productName,
+			numProducts,
+			totalPrice,
+			imageURL
+		);
+
+		res.status(201).json({ message: 'Cart created successfully' });
+	} catch (error) {
+		console.error(error);
+		res.status(500).json({ error: 'Internal Server Error' });
+	}
 });
 
 app.get('/monitor', (req, res) => {
-  // res.sendFile(__dirname+'/index.html');
-  res.sendFile('monitor.html', { root: '.' });
+	// res.sendFile(__dirname+'/index.html');
+	res.sendFile('monitor.html', { root: '.' });
 });
 
 const orders = [
-  { orderId: 1, status: "Pending", totalAmount: 100 },
-  { orderId: 2, status: "Shipped", totalAmount: 100 },
-  { orderId: 3, status: "Pending", totalAmount: 100 },
+	{ orderId: 1, status: 'Pending', totalAmount: 100 },
+	{ orderId: 2, status: 'Shipped', totalAmount: 100 },
+	{ orderId: 3, status: 'Pending', totalAmount: 100 },
 ];
 
-// const sampleproducts = [
-//   { productId: 1, name: "Lightsaber", description: "A jedi lightsaber", price: 100 },
-//   { productId: 2, name: "Blaster", description: "A blaster from star wars", price: 200 },
-//   { productId: 3, name: "Droid", description: "A droid from star wars", price: 300 },
-// ];
+const products = [
+	{
+		productId: 1,
+		name: 'Lightsaber',
+		description: 'A jedi lightsaber',
+		price: 100,
+	},
+	{
+		productId: 2,
+		name: 'Blaster',
+		description: 'A blaster from star wars',
+		price: 200,
+	},
+	{
+		productId: 3,
+		name: 'Droid',
+		description: 'A droid from star wars',
+		price: 300,
+	},
+];
 
 app.get('/monitor_order', (req, res) => {
-  console.log("monitor_order");
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify(orders));
+	console.log('monitor_order');
+	res.writeHead(200, { 'Content-Type': 'application/json' });
+	res.end(JSON.stringify(orders));
 });
 
-
-app.get('/product_list', async (req, res) => {
-  // Dislay 20 list of products from database
-  let productids= [1,2,3,4];
-  let products=[];
-  for (let i = 0; i < productids.length; i++) {
-    let product= await getPRODUCT(productids[i]);
-    products.push(product);
-  }
-  // console.log(products);
-  // console.log(sampleproducts);
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify(products));
+app.get('/product_list', (req, res) => {
+	res.writeHead(200, { 'Content-Type': 'application/json' });
+	res.end(JSON.stringify(products));
 });
 
-app.get('/fetch_cart', async (req, res) => {
-  let items= [];
-  let cartID = 5; // Currently works for a cart
-  let carts = await getCART(cartID);
-  let products= await getPRODUCT(carts['productID']);
-  items.push(products);
-  // console.log(products);
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-  res.end(JSON.stringify(items));
+app.get('/cart_list', (req, res) => {
+	res.writeHead(200, { 'Content-Type': 'application/json' });
+	res.end(JSON.stringify(products));
 });
 
+app.get('/fetch_cart', (req, res) => {
+	res.writeHead(200, { 'Content-Type': 'application/json' });
+	res.end(JSON.stringify(products));
+});
 
 app.get('/cart', (req, res) => {
-  // res.sendFile(__dirname+'/index.html');
-  res.sendFile('cart.html', { root: '.' });
-  // res.writeHead(200, { 'Content-Type': 'application/json' });
-        // res.end(JSON.stringify(products));
+	// res.sendFile(__dirname+'/index.html');
+	res.sendFile('cart.html', { root: '.' });
+	// res.writeHead(200, { 'Content-Type': 'application/json' });
+	// res.end(JSON.stringify(products));
 });
 
 app.get('/account', (req, res) => {
-  // res.sendFile(__dirname+'/index.html');
-  res.sendFile('account.html', { root: '.' });
-  // res.writeHead(200, { 'Content-Type': 'application/json' });
-        // res.end(JSON.stringify(products));
+	// res.sendFile(__dirname+'/index.html');
+	res.sendFile('account.html', { root: '.' });
+	// res.writeHead(200, { 'Content-Type': 'application/json' });
+	// res.end(JSON.stringify(products));
 });
-
-//CONTRIBUTION OF Santosh Pandey END
